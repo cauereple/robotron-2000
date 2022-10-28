b3ec21a683f7cf4fde31c8f530b93f655006c006
Cauê Reple <caue-reple@hotmail.com>
2022-10-28 16:26:22 +0200

    adding arms value

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
-const robotron = document.querySelector("#robotron");
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
+
+somar.addEventListener("click", (evento) => {
+   braco.value = parseInt(braco.value) + 1;
+})
+
+subtrair.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) - 1;
+ })
 
 // podemos fazer dessa forma, chamando a nossa funçao
 //robotron.addEventListener("click", dizOi);
 
 // ou podemos fazer com uma funçao anonima dentro do nosso evento
-robotron.addEventListener("click", (evento) => {
-    console.log("cliquei no robo");
-})
+// robotron.addEventListener("click", (evento) => {
+//     console.log("cliquei no robo");
+// })
 
-function dizOi(nome) {
-    console.log("Oi " + nome)
-    console.log("oi");
-}
+// function dizOi(nome) {
+//     console.log("Oi " + nome)
+//     console.log("oi");
+// }
